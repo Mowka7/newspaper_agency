b772f41380f2c8bd2114e1eef14f384fe0bc5bc9
Mowka7 <dadzaiosamu5@gmail.com>
2022-11-16 00:08:27 +0000

    Added models and migrations

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from newspaper_agency import settings
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=70)
+    content = models.TextField()
+    published_date = models.DateField(auto_now_add=True)
+    topics = models.ManyToManyField(Topic, related_name="newspapers")
+    publishers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self) -> str:
+        return self.title
